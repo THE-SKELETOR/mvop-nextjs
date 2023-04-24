b627116145cd0fded80cc174daca63f3f89df420
THE-SKELETOR <90837383+THE-SKELETOR@users.noreply.github.com>
2023-04-24 14:46:36 +0200

    what kind of mouses are good? im a sucker for A4te

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import '@/styles/globals.css'
+import Link from 'next/link'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return  (
+    <div>
+      <header>
+        <div className="flex justify-center align-middle h-10">
+          This is the header. Here are some links!
+          <div className="flex justify-around w-6/12">
+            <div>
+              <Link href="/">Home</Link>
+            </div>
+            <div>
+              <Link href="/about">About</Link>
+            </div>
+          </div>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </div>
+  )
 }
